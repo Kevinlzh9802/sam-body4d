8639c2055e29e78f4a56314dfc4f1a6d9246fa6e
Kevinlzh9802 <zonghuan_li@outlook.com>
2026-01-03 13:32:44 +0100

    use innovation for now

--- job_scripts/inference_test.sh
+++ job_scripts/inference_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=insy,general # Request partition. Default is 'general' 
+#SBATCH --partition=innovation # Request partition. Default is 'general' 
 #SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
 #SBATCH --time=2:00:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --cpus-per-task=1
