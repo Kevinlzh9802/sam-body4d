c28fc45c8c1965ecff1da1e77c5fe59d96f442ce
Kevinlzh9802 <zonghuan_li@outlook.com>
2026-01-03 22:21:45 +0100

    choose 1357 for boxes

--- infer_video.py
+++ infer_video.py
@@ -526,7 +526,7 @@ Examples:
     
     # bboxes_kps_data = load_bbox_kp("/mnt/neon/zonghuan/data/sam4d_body/inputs/bboxes_kps_refined", "428")
     bboxes_kps_data = load_bbox_kp("/mnt/data/sam4d_body/inputs/bboxes_kps_refined", "428")
-    selected_boxes = [2, 4, 6, 8]
+    selected_boxes = [1, 3, 5, 7]
     for obj_id in selected_boxes:
         bbox = bboxes_kps_data[0]['bboxes'][obj_id]
         rel_box = bbox / [width, height, width, height]
