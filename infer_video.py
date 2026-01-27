4e99ca28b14f402be775f5a293e3957b7db0e66a
Kevinlzh9802 <zonghuan_li@outlook.com>
2026-01-27 20:19:27 +0100

    include read intrinsics new in infer_video

--- infer_video.py
+++ infer_video.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 from omegaconf import OmegaConf
 import pickle
 import json
-from dataset.undistortion import read_camera_intrinsics_new
+# from dataset.undistortion import read_camera_intrinsics_new
 # from tools.load_bbox_kp import load_bbox_kp
 
 # Add model paths
@@ -98,6 +98,21 @@ def read_video_metadata(path: str):
     cap.release()
     return fps, total
 
+
+def read_camera_intrinsics_new(intrinsic_file: str):
+    with open(intrinsic_file, "r") as f:
+        intrinsic_data = json.load(f)
+        params = intrinsic_data['Calibration']['cameras'][0]['model']['ptr_wrapper']['data']['parameters']
+
+        f = params['f']['val']
+        cx = params['cx']['val']
+        cy = params['cy']['val']
+        
+        K = np.array([[f, 0, cx], [0, f, cy], [0, 0, 1]])
+        ks = [params[f'k{i}']['val'] for i in range(1, 5)]
+        dist_coeffs = np.array(ks)
+
+    return K, dist_coeffs
 
 def mask_generation(video_path: str, predictor, inference_state, output_dir, fps, out_obj_ids):
     """
