91288b678dfaa9029efd07ce1d093f957e8c391a
Kevinlzh9802 <zonghuan_li@outlook.com>
2026-04-19 17:52:59 +0200

    change resources

--- job_scripts/smooth_meshes_batch_ingroup.sh
+++ job_scripts/smooth_meshes_batch_ingroup.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4000M
+#SBATCH --mem-per-cpu=3000M
 #SBATCH --mail-type=END
 #SBATCH --account=research-eemcs-insy
 #SBATCH --output=/home/zli33/slurm_outputs/sam_4d_body/slurm_%j.out
