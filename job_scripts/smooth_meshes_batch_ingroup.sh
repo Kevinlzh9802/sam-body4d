5f1d53e6b0a519283462c03b2785a5da50d57d34
Kevinlzh9802 <zonghuan_li@outlook.com>
2026-04-19 17:52:37 +0200

    change ingroup resources

--- job_scripts/smooth_meshes_batch_ingroup.sh
+++ job_scripts/smooth_meshes_batch_ingroup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="smooth_meshes_batch"
-#SBATCH --partition=gpu
-#SBATCH --time=18:00:00
+#SBATCH --partition=compute-p2
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4000M
-#SBATCH --gpus-per-task=1
 #SBATCH --mail-type=END
 #SBATCH --account=research-eemcs-insy
 #SBATCH --output=/home/zli33/slurm_outputs/sam_4d_body/slurm_%j.out
